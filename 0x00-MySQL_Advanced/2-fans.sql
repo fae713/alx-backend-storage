dafeb07fd1b181397dc7ce821a3bf122fd761f16
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-15 17:34:45 +0000

    dubugging

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -6,7 +6,7 @@ SET @row_number = 0;
 SET @nb_fans = 0;
 
 SELECT origin, (@nb_fans := @nb_fans + SUM(fans)) AS nb_fans, 
-       (@row_number := @row_number + 1) AS ranking
+     --  (@row_number := @row_number + 1) AS ranking
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
