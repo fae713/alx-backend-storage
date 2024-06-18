78b909b7f0b5cfeab8cc021e3350c6f02aae6f39
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-18 12:38:25 +0000

    task 101 debugging

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,18 +1,17 @@
 --   a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
 
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
-
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE avg_weighted_score DECIMAL(10,2);
-    
-    SELECT AVG(score * weight) INTO avg_weighted_score
-    FROM users u
-    JOIN corrections c ON u.id = c.user_id
-    JOIN projects p ON c.project_id = p.id;
-    
-    UPDATE users
-    SET average_score = avg_weighted_score;
+    UPDATE users AS U
+    INNER JOIN (
+        SELECT U.id, SUM(C.score * P.weight) / SUM(P.weight) AS w_avg
+        FROM users AS U
+        JOIN corrections AS C ON U.id = C.user_id
+        JOIN projects AS P ON C.project_id = P.id
+        GROUP BY U.id
+    ) AS WA ON U.id = WA.id
+    SET U.average_score = WA.w_avg;
 END $$
-
 DELIMITER ;
