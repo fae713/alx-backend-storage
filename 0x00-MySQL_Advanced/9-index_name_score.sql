261e7e993147f6f3f9b1be2e7a3b55350aa2e77e
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-18 10:06:07 +0000

    Task 8 and 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -4,7 +4,6 @@
 
 
 ALTER TABLE names
-ADD COLUMN name_first CHAR(1) GENERATED ALWAYS AS (SUBSTRING(name, 1, 1)) STORED;
 ADD COLUMN score_value DOUBLE GENERATED ALWAYS AS (score) STORED;
 
-CREATE INDEX idx_name_first_score ON names (name_first, score);
+CREATE INDEX idx_name_first_score ON names (name, score);
