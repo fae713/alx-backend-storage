386aff96cb57215636c5a8b1ae7a5e12524a0cb6
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-20 14:17:13 +0000

    Task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -36,6 +36,26 @@ class Cache:
             return method(self, *args, **kwargs)
         return wrapper
 
+    def call_history(method: Callable) -> Callable:
+        """
+        A decorator to store the history of inputs and outputs for a function.
+        """
+        # Construct keys for inputs and outputs based on the method's qualified name
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        @wraps(method)
+        def history_wrapper(self, *args, **kwargs):
+            self._redis.rpush(input_key, str(args))
+
+            result = method(self, *args, **kwargs)
+
+            self._redis.rpush(output_key, str(result))
+
+            return result
+        return history_wrapper
+
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
