acd3c146da1fde1e1655be5e39e10f60f74f2111
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-20 08:45:37 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@
 """
 
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 import uuid
 
 
@@ -29,3 +29,53 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, int, float, bytes]:
+        """
+        - method that take a key str and an optional Callable(fn).
+
+        - params:
+          key -> The key of the data to retrieve.
+          fn: -> An optional callable to convert the retrieved data.
+          return -> The retrieved data.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            try:
+                if fn is not None:
+                    return fn(data)
+                else:
+                    return data
+            except Exception as e:
+                print(f"Error converting data: {e}")
+                return None
+        else:
+            return None
+
+    def get_str(self, key: str) -> str:
+        """
+        This is a method that will automatically parametrize Cache.get
+        with the correct conversion function.
+        """
+        return self.get(key, lambda b: b.decode("utf-8"))
+
+    def get_int(self, key: int) -> int:
+        """
+        This is a method that will automatically parametrize Cache.get
+        with the correct conversion function.
+        """
+        return self.get(key, int)
+
+
+if __name__ == "__main__":
+    cache = Cache()
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
