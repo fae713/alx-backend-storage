ed9bf6445356d562a08ea25caf25c3cef69b3a11
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-20 11:24:28 +0000

    Task 1 debugging

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,9 @@
 import redis
 from typing import Union, Callable, Optional
 import uuid
+from functools import wraps
 
+SENTINEL_NONE = "N/A"
 
 class Cache:
     """
@@ -26,12 +28,15 @@ class Cache:
             param: data -> The data to be strored in redis.
                    return -> A string representing the key.
         """
+        if data is None:
+            data = SENTINEL_NONE
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-
+    
     def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[str, int, float, bytes]:
+            fn: Optional[Callable[[bytes], Union[str, bytes, int, float]]] = 
+            None) -> Union[str, bytes, int, float]:
         """
         - method that take a key str and an optional Callable(fn).
 
@@ -41,7 +46,9 @@ class Cache:
           return -> The retrieved data.
         """
         data = self._redis.get(key)
-        if data is None:
+        if data == SENTINEL_NONE:
+            return None
+        elif data is None:
             try:
                 if fn is not None:
                     return fn(data)
@@ -53,23 +60,47 @@ class Cache:
         else:
             return None
 
-    def get_str(self, key: str) -> str:
+    def get(self, key: str,
+            fn: Optional[Callable[[bytes], Union[str, bytes, int, float]]] =
+            None) -> Union[str, bytes, int, float]:
         """
-        This is a method that will automatically parametrize Cache.get
-        with the correct conversion function.
-        """
-        return self.get(key, lambda b: b.decode("utf-8"))
+        - method that take a key str and an optional Callable(fn).
 
-    def get_int(self, key: int) -> int:
+        - params:
+          key -> The key of the data to retrieve.
+          fn: -> An optional callable to convert the retrieved data.
+          return -> The retrieved data.
         """
-        This is a method that will automatically parametrize Cache.get
-        with the correct conversion function.
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
         """
-        return self.get(key, int)
+        Method that will automatically parametrize Cache.get
+        with the correct conversion function for strings
+        """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Method that will automatically parametrize Cache.get
+        with the correct conversion function for integers
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
 
 
+# Test block
 if __name__ == "__main__":
     cache = Cache()
+
     TEST_CASES = {
         b"foo": None,
         123: int,
@@ -79,3 +110,8 @@ if __name__ == "__main__":
     for value, fn in TEST_CASES.items():
         key = cache.store(value)
         assert cache.get(key, fn=fn) == value
+    print("All tests passed successfully.")
+
+    cache.store("foo")
+    cache.store("bar")
+    cache.store(42)
